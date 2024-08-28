1b182cd9d69dc77bca547331ff568ca94f189dac
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-28 17:44:54 +0300

    pipes

---
+++ src/bootstrap/pipes.bootstrap.ts
@@ -0,0 +1,5 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
+export const bootstrapPipes = (app: INestApplication) => {
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+};
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { bootstrapPipes } from './bootstrap/pipes.bootstrap';
 import { bootstrapSwagger } from './bootstrap/swagger.bootstrap';
 
 export async function bootstrap() {
   const app = await NestFactory.create<INestApplication>(AppModule);
 
   bootstrapSwagger(app);
+  bootstrapPipes(app);
 
   await app.listen(2000);
 }
