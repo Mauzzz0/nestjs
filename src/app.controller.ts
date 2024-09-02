c5a29807f3d23de91d3cda43b0316a143293ce50
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-09-02 14:29:03 +0300

    Добавлен Logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,14 @@
-import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
-import { Controller, Get, Post, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Logger, Post, Query } from '@nestjs/common';
 
 import { ExampleQueryDto } from './dto/exampleQueryDto';
 
-@ApiTags('asd')
 @Controller()
 export class AppController {
-  constructor(private readonly amqpConnection: AmqpConnection) {}
+  private readonly logger = new Logger(AppController.name);
 
   @Post('')
   async post(@Query() query: ExampleQueryDto) {
+    this.logger.log('Пришел запрос POST /');
     return query;
-  }
-
-  @Get('2')
-  async push2() {
-    return this.amqpConnection.publish('nestjs-exchange-1', 'nestjs-2', 'This message 2!!');
   }
 }
